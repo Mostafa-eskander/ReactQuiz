2a21c87fb75ce08f14cc7a63ecfff1710f519d9e
hagermahmoudgad-create <hagermahmoudgad@gmail.com>
2026-02-15 21:08:45 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Header from './comonents/Header.jsx';
+import Quiz from './comonents/Quiz.jsx';
+
+function App() {
+    return(
+        <>
+            <Header />
+            <main>
+                <Quiz />
+            </main>
+        </>
+    )
+}
+
+export default App;
